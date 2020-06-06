3a72a0ae02678290f653d4a147486175fb4e2ef9
Brandon Ferrer <brandonferrer@peachjar.com>
2020-06-05 21:25:13 -0700

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+function getWeatherData(setAppState: any, axios: any): void {
+  let data = localStorage.getItem("data");
+
+  if (data) {
+    setAppState(data);
+  } else {
+    data = axios.get();
+    setAppState(data);
+  }
+}
+
+const App = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    getWeatherData(setData, axios);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Foo</h1>
     </div>
   );
-}
+};
 
 export default App;
